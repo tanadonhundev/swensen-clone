495e56a549ffa1cdf5af8c3ade1eae54643921e1
Tanadon Hunsadee <tanadonhun.dev@gmail.com>
2025-04-16 20:56:19 +0700

    ss

--- drizzle/schema.ts
+++ drizzle/schema.ts
@@ -1,128 +1,75 @@
-import {
-  mysqlTable,
-  primaryKey,
-  varchar,
-  text,
-  timestamp,
-  int,
-  decimal,
-  unique,
-  mysqlEnum,
-  tinyint,
-} from "drizzle-orm/mysql-core";
-import { sql } from "drizzle-orm";
+import { mysqlTable, varchar, text, timestamp, int, decimal, unique, mysqlEnum, tinyint } from "drizzle-orm/mysql-core"
 
-export const account = mysqlTable(
-  "account",
-  {
-    id: varchar({ length: 36 }).notNull(),
-    accountId: text("account_id").notNull(),
-    providerId: text("provider_id").notNull(),
-    userId: varchar("user_id", { length: 36 })
-      .notNull()
-      .references(() => user.id, { onDelete: "cascade" }),
-    accessToken: text("access_token"),
-    refreshToken: text("refresh_token"),
-    idToken: text("id_token"),
-    accessTokenExpiresAt: timestamp("access_token_expires_at", {
-      mode: "string",
-    }),
-    refreshTokenExpiresAt: timestamp("refresh_token_expires_at", {
-      mode: "string",
-    }),
-    scope: text(),
-    password: text(),
-    createdAt: timestamp("created_at", { mode: "string" }).notNull(),
-    updatedAt: timestamp("updated_at", { mode: "string" }).notNull(),
-  },
-  (table) => [primaryKey({ columns: [table.id], name: "account_id" })]
-);
 
-export const productCategory = mysqlTable(
-  "product_category",
-  {
-    id: int().autoincrement().notNull(),
-    categoryName: varchar("category_name", { length: 100 }).notNull(),
-    createdAt: timestamp("created_at", { mode: "string" }).default(
-      sql`(now())`
-    ),
-    updatedAt: timestamp("updated_at", { mode: "string" })
-      .default(sql`(now())`)
-      .onUpdateNow(),
-  },
-  (table) => [primaryKey({ columns: [table.id], name: "product_category_id" })]
-);
+export const account = mysqlTable("account", {
+	id: varchar({ length: 36 }).notNull(),
+	accountId: text("account_id").notNull(),
+	providerId: text("provider_id").notNull(),
+	userId: varchar("user_id", { length: 36 }).notNull().references(() => user.id, { onDelete: "cascade" } ),
+	accessToken: text("access_token").default('NULL'),
+	refreshToken: text("refresh_token").default('NULL'),
+	idToken: text("id_token").default('NULL'),
+	accessTokenExpiresAt: timestamp("access_token_expires_at", { mode: 'string' }).default('current_timestamp()').notNull(),
+	refreshTokenExpiresAt: timestamp("refresh_token_expires_at", { mode: 'string' }).default('current_timestamp()'),
+	scope: text().default('NULL'),
+	password: text().default('NULL'),
+	createdAt: timestamp("created_at", { mode: 'string' }).default('current_timestamp()').notNull(),
+	updatedAt: timestamp("updated_at", { mode: 'string' }).default('current_timestamp()').notNull(),
+});
 
-export const productItem = mysqlTable(
-  "product_item",
-  {
-    id: int().autoincrement().notNull(),
-    categoryId: int("category_id")
-      .notNull()
-      .references(() => productCategory.id),
-    description: text(),
-    price: decimal({ precision: 10, scale: 2 }).notNull(),
-    imageName: text("image_name").notNull(),
-    createdAt: timestamp("created_at", { mode: "string" }).default(
-      sql`(now())`
-    ),
-    updatedAt: timestamp("updated_at", { mode: "string" })
-      .default(sql`(now())`)
-      .onUpdateNow(),
-  },
-  (table) => [primaryKey({ columns: [table.id], name: "product_item_id" })]
-);
+export const productCategory = mysqlTable("product_category", {
+	id: int().autoincrement().notNull(),
+	categoryName: varchar("category_name", { length: 100 }).notNull(),
+	createdAt: timestamp("created_at", { mode: 'string' }).default('current_timestamp()').notNull(),
+	updatedAt: timestamp("updated_at", { mode: 'string' }).default('current_timestamp()').notNull(),
+});
 
-export const session = mysqlTable(
-  "session",
-  {
-    id: varchar({ length: 36 }).notNull(),
-    expiresAt: timestamp("expires_at", { mode: "string" }).notNull(),
-    token: varchar({ length: 255 }).notNull(),
-    createdAt: timestamp("created_at", { mode: "string" }).notNull(),
-    updatedAt: timestamp("updated_at", { mode: "string" }).notNull(),
-    ipAddress: text("ip_address"),
-    userAgent: text("user_agent"),
-    userId: varchar("user_id", { length: 36 })
-      .notNull()
-      .references(() => user.id, { onDelete: "cascade" }),
-  },
-  (table) => [
-    primaryKey({ columns: [table.id], name: "session_id" }),
-    unique("session_token_unique").on(table.token),
-  ]
-);
+export const productItem = mysqlTable("product_item", {
+	id: int().autoincrement().notNull(),
+	categoryId: int("category_id").notNull().references(() => productCategory.id, { onDelete: "cascade" } ),
+	title: text().default('NULL'),
+	price: decimal({ precision: 10, scale: 2 }).notNull(),
+	imageName: text("image_name").notNull(),
+	createdAt: timestamp("created_at", { mode: 'string' }).default('current_timestamp()').notNull(),
+	updatedAt: timestamp("updated_at", { mode: 'string' }).default('current_timestamp()').notNull(),
+});
 
-export const user = mysqlTable(
-  "user",
-  {
-    id: varchar({ length: 36 }).notNull(),
-    name: text().notNull(),
-    email: varchar({ length: 255 }).notNull(),
-    emailVerified: tinyint("email_verified").notNull(),
-    image: text(),
-    createdAt: timestamp("created_at", { mode: "string" }).notNull(),
-    updatedAt: timestamp("updated_at", { mode: "string" }).notNull(),
-    role: mysqlEnum(["user", "admin"]).default("user"),
-    birthDate: text(),
-    gender: text(),
-    phone: text(),
-  },
-  (table) => [
-    primaryKey({ columns: [table.id], name: "user_id" }),
-    unique("user_email_unique").on(table.email),
-  ]
-);
+export const session = mysqlTable("session", {
+	id: varchar({ length: 36 }).notNull(),
+	expiresAt: timestamp("expires_at", { mode: 'string' }).default('current_timestamp()').notNull(),
+	token: varchar({ length: 255 }).notNull(),
+	createdAt: timestamp("created_at", { mode: 'string' }).default('current_timestamp()').notNull(),
+	updatedAt: timestamp("updated_at", { mode: 'string' }).default('current_timestamp()').notNull(),
+	ipAddress: text("ip_address").default('NULL'),
+	userAgent: text("user_agent").default('NULL'),
+	userId: varchar("user_id", { length: 36 }).notNull().references(() => user.id, { onDelete: "cascade" } ),
+},
+(table) => [
+	unique("session_token_unique").on(table.token),
+]);
 
-export const verification = mysqlTable(
-  "verification",
-  {
-    id: varchar({ length: 36 }).notNull(),
-    identifier: text().notNull(),
-    value: text().notNull(),
-    expiresAt: timestamp("expires_at", { mode: "string" }).notNull(),
-    createdAt: timestamp("created_at", { mode: "string" }),
-    updatedAt: timestamp("updated_at", { mode: "string" }),
-  },
-  (table) => [primaryKey({ columns: [table.id], name: "verification_id" })]
-);
+export const user = mysqlTable("user", {
+	id: varchar({ length: 36 }).notNull(),
+	name: text().notNull(),
+	email: varchar({ length: 255 }).notNull(),
+	birthDate: text().default('NULL'),
+	gender: text().default('NULL'),
+	phone: text().default('NULL'),
+	emailVerified: tinyint("email_verified").notNull(),
+	image: text().default('NULL'),
+	createdAt: timestamp("created_at", { mode: 'string' }).default('current_timestamp()').notNull(),
+	updatedAt: timestamp("updated_at", { mode: 'string' }).default('current_timestamp()').notNull(),
+	role: mysqlEnum(['user','admin']).default("user"),
+},
+(table) => [
+	unique("user_email_unique").on(table.email),
+]);
+
+export const verification = mysqlTable("verification", {
+	id: varchar({ length: 36 }).notNull(),
+	identifier: text().notNull(),
+	value: text().notNull(),
+	expiresAt: timestamp("expires_at", { mode: 'string' }).default('current_timestamp()').notNull(),
+	createdAt: timestamp("created_at", { mode: 'string' }).default('current_timestamp()').notNull(),
+	updatedAt: timestamp("updated_at", { mode: 'string' }).default('current_timestamp()').notNull(),
+});
--- src/db/schema.ts
+++ src/db/schema.ts
@@ -18,8 +18,8 @@ export const user = mysqlTable("user", {
   phone: text("phone"),
   emailVerified: boolean("email_verified").notNull(),
   image: text("image"),
-  createdAt: timestamp("created_at").notNull(),
-  updatedAt: timestamp("updated_at").notNull(),
+  createdAt: timestamp("created_at").defaultNow(),
+  updatedAt: timestamp("updated_at").defaultNow().onUpdateNow(),
   role: mysqlEnum(["user", "admin"]).default("user"),
 });
 
@@ -27,8 +27,8 @@ export const session = mysqlTable("session", {
   id: varchar("id", { length: 36 }).primaryKey(),
   expiresAt: timestamp("expires_at").notNull(),
   token: varchar("token", { length: 255 }).notNull().unique(),
-  createdAt: timestamp("created_at").notNull(),
-  updatedAt: timestamp("updated_at").notNull(),
+  createdAt: timestamp("created_at").defaultNow(),
+  updatedAt: timestamp("updated_at").defaultNow().onUpdateNow(),
   ipAddress: text("ip_address"),
   userAgent: text("user_agent"),
   userId: varchar("user_id", { length: 36 })
@@ -47,11 +47,11 @@ export const account = mysqlTable("account", {
   refreshToken: text("refresh_token"),
   idToken: text("id_token"),
   accessTokenExpiresAt: timestamp("access_token_expires_at"),
-  refreshTokenExpiresAt: timestamp("refresh_token_expires_at"),
+  refreshTokenExpiresAt: timestamp("refresh_oken_expires_at"),
   scope: text("scope"),
   password: text("password"),
-  createdAt: timestamp("created_at").notNull(),
-  updatedAt: timestamp("updated_at").notNull(),
+  createdAt: timestamp("created_at").defaultNow(),
+  updatedAt: timestamp("updated_at").defaultNow().onUpdateNow(),
 });
 
 export const verification = mysqlTable("verification", {
@@ -59,8 +59,8 @@ export const verification = mysqlTable("verification", {
   identifier: text("identifier").notNull(),
   value: text("value").notNull(),
   expiresAt: timestamp("expires_at").notNull(),
-  createdAt: timestamp("created_at"),
-  updatedAt: timestamp("updated_at"),
+  createdAt: timestamp("created_at").defaultNow(),
+  updatedAt: timestamp("updated_at").defaultNow().onUpdateNow(),
 });
 
 // ProductCategory Table
